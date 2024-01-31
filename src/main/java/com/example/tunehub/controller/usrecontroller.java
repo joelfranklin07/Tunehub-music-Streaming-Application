71d3ea39c91a9dae68298affdff98c509bb38f5b
JOEL FRANKLIN R <JOEL FRANKLIN R@HP-PAVILION>
2024-01-31 12:28:53 +0530

    project

--- src/main/java/com/example/tunehub/controller/usrecontroller.java
+++ src/main/java/com/example/tunehub/controller/usrecontroller.java
@@ -3,20 +3,23 @@ package com.example.tunehub.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.example.tunehub.entities.Logindata;
 import com.example.tunehub.entities.song;
 import com.example.tunehub.entities.users;
 import com.example.tunehub.services.Songservice;
 import com.example.tunehub.services.UsersService;
 import jakarta.servlet.http.HttpSession;
-import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+@CrossOrigin("*")
+@RestController
 public class usrecontroller 
 {
 	@Autowired
@@ -38,10 +41,12 @@ public class usrecontroller
 		return "login";
 	}
 	@PostMapping("/validate")
-	public String validate(@RequestParam("email")String email,
-			@RequestParam("password") String password,
-			HttpSession session,Model model) {
+	public String validate(@RequestBody Logindata data,HttpSession session,Model model) 
+	{	
+		System.out.println("call recived");
 		
+		String email=data.getEmail();
+		String password=data.getPassword();
 		if(service.validateUser(email,password) == true) {
 			String role = service.getRole(email);
 			
